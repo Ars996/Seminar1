bb16993917225ac1ab3ef2114251db223bc3c46a
Ars <79503943422@ya.ru>
2022-08-19 18:49:47 +0800

    Решили задачу2

--- HW002/Program.cs
+++ HW002/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WriteLine("Введите три числа:");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+int numС = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+if (numB > max) max=numB;
+if (numС > max) max=numС;
+  Console.WriteLine("Максимальное число: "+ max);
+
