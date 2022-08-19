751b71254d9d2e3f7ceb9970b1d98671a4caf33d
Ars <79503943422@ya.ru>
2022-08-19 19:25:53 +0800

    задача решена

--- HW004/Program.cs
+++ HW004/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negNum = num* -1;
+
+while (negNum<=num) {
+    Console.Write(negNum +", " );
+    negNum++;
+}
